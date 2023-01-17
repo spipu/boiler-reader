6a73cbd5663a897c57e11110f4b2b897cb669075
spipu <spipu45@gmail.com>
2023-01-17 23:23:19 +0100

    add buffer delete

--- website/src/Controller/MainController.php
+++ website/src/Controller/MainController.php
@@ -6,10 +6,13 @@ namespace App\Controller;
 use App\Repository\BufferRepository;
 use App\Ui\BufferForm;
 use App\Ui\BufferGrid;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Spipu\UiBundle\Exception\UiException;
 use Spipu\UiBundle\Service\Ui\GridFactory;
 use Spipu\UiBundle\Service\Ui\ShowFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -70,5 +73,45 @@ class MainController extends AbstractController
                 'manager' => $manager
             ]
         );
+    }
+
+
+    /**
+     * @Route("/delete/{id}", name="app_buffer_delete", methods="DELETE")
+     * @param Request $request
+     * @param BufferRepository $bufferRepository
+     * @param EntityManagerInterface $entityManager
+     * @param int $id
+     * @return Response
+     */
+    public function delete(
+        Request $request,
+        BufferRepository $bufferRepository,
+        EntityManagerInterface $entityManager,
+        int $id
+    ): Response {
+        $resource = $bufferRepository->findOneBy(['id' => $id]);
+        if (!$resource) {
+            throw $this->createNotFoundException();
+        }
+
+        if (!$this->isCsrfTokenValid('delete_buffer_' . $resource->getId(), $request->request->get('_token'))) {
+            $this->addFlash('danger', 'Token error');
+            return $this->redirectToRoute('app_home');
+        }
+
+        try {
+            $entityManager->remove($resource);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Buffer deleted');
+        } catch (Exception $e) {
+            $this->addFlash('danger', $e->getMessage());
+        }
+
+        $entityManager->remove($resource);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
     }
 }
